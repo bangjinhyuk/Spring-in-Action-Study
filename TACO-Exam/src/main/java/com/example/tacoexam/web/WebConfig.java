5e570838e6870914dad5ecd1e980e4497cf41486
bangjinhyuk <jinhyuk0103@gmail.com>
2021-11-25 17:51:17 +0900

    2.4 WebConfig 추가

---
+++ TACO-Exam/src/main/java/com/example/tacoexam/web/WebConfig.java
@@ -0,0 +1,17 @@
+package com.example.tacoexam.web;
+
+/**
+ * Created by bangjinhyuk on 2021/11/25.
+ */
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("home");
+    }
+}
